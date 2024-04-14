2c04f0cdfc45728689233b9eabab961f2d0dbe78
2H-K <m51777888@163.com>
2024-04-14 17:16:33 +0800

    feat: add new feature

--- mover/Awoodwhale/code/task6/main.js
+++
@@ -1,6 +0,0 @@
-import { NAVISDKClient } from 'navi-sdk'
-import { Sui } from 'navi-sdk/dist/address.js';
-
-const client = new NAVISDKClient({mnemonic: '???', networkType: "mainnet"})
-const account = client.accounts[0]
-await account.depositToNavi(Sui, 1e9)
--- mover/Zebra6-web/notes/readme.md
+++
@@ -1,3 +0,0 @@
-## move学习笔记4.11 基本命令
-检查当前的环境变量 sui client envs
-切换网络命令 sui client switch --env <ALIAS>
