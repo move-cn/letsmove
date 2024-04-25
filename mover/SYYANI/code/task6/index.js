89bff3354149745c3913c502fab3367a4780e6a0
SYYANI <40732861+SYYANI@users.noreply.github.com>
2024-04-25 15:23:38 +0800

    update readme

--- mover/SYYANI/code/task6/index.js
+++ mover/SYYANI/code/task6/index.js
@@ -1,7 +1,7 @@
 import { NAVISDKClient } from 'navi-sdk'
 import { Sui } from 'navi-sdk/dist/address.js';
 
-const mnemonic = 'cement add level property diagram final about toss stumble pony fat pilot';
+const mnemonic = '';
 
 const client = new NAVISDKClient({mnemonic, numberOfAccounts: 1});
 
@@ -10,6 +10,6 @@ const account = client.accounts[0];
 let amount = 1e9;
 
 account.depositToNavi(Sui, amount);
-// account.withdraw(Sui, amount)
+account.withdraw(Sui, amount)
 
 console.log(account.getAllCoins());
