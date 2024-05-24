437ee62ef58c6cf8170c76c2d832565cc4925ba1
RobbieChen <chenhang0801@qq.com>
2024-05-24 15:04:05 +0800

    Add files via upload

---
+++ mover/RobbieChen/code/task3/sources/call.sh
@@ -0,0 +1,5 @@
+sui client call --gas-budget 100000000\
+ --package 0xb6c2ff2ca71c67384a666b0a06af03253c738fa39124fa606c5693c219eea412 \
+ --module nft \
+ --function transferNFT \
+ --args 0x867f9edfd636525fdf2eb3b17230dd0679e8d0ebd51d29fee27f3bc9c57bfe63 0x7b8e0864967427679b4e129f79dc332a885c6087ec9e187b53451a9006ee15f2
