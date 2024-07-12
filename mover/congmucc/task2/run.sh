93dcf4c810fb41603f254dfb1deaa5e49874de36
congmu <2562907972@qq.com>
2024-07-12 23:39:23 +0800

    congmu/task2

---
+++ mover/congmucc/task2/run.sh
@@ -0,0 +1,5 @@
+sui client call --package 0x2 \
+--module coin --function mint_and_transfer \
+--args 0xfd92912b3b7fd159afb39329c1ccdd8719c7a6042fce9ce1106f688dcca2045d \
+100000000 0xfabe32506438a111fa9cc7db561bea1e47121ef51cdb4e6b73658e90e60a88f5 \
+--type-args "0x0a85af72122aa89d98ff4720f7ef7faf370397719270635602beeac7748dc78c::my_coin::MY_COIN"
