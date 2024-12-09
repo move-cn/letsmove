54dc2cd9baacfb9a51e9db751a4c4f5f7e16b725
yizuo <yizuo998@gmail.com>
2024-12-09 11:37:44 +0800

    fix：修改task-2、task-3为主网

--- mover/yizuo66/code/task-2/yizuo_coin/yizuo_call.sh
+++ mover/yizuo66/code/task-2/yizuo_coin/yizuo_call.sh
@@ -1,19 +1,6 @@
  sui client call --package 0x2 \
   --module coin \
   --function mint_and_transfer \
-  --type-args 0xcc61159ac83c2ea47d730268f85a25081a79d86a7dec7aff4e2cd9b076edb608::yizuo::YIZUO \
-   --args   0xe5757f08cc942536816c83c9a73e863a496719e19b9d8349df9d1f331204277d 1000000000 0xae8da2da7d08b7591709a6e6ff95e1a64067526087749aa4ecf8bb5cb4b8e273
+  --type-args 0x2076506802cfdf80533f90e9b973fe5d2d258c3c86ce57249f91a017b5d9877c::yizuo::YIZUO \
+   --args   0xe2f304c059d687020ddc5b2590c811d1b1fd4e9a1e180dca5ddbfbad55adb395 1000000000 0x05df3dd3f3c4836af29c1df7467dff16dadf29b4843373a1726e0b1575afb8a0
 
-
- sui client call --package 0x2 \
-  --module coin \
-  --function mint_and_transfer \
-  --type-args 0x1484c74770daea990bca0821296b7c740d83e484c4e342dafc4aed68b5e0e6b0::yizuo::YIZUO \
-   --args   0x3104577ea239b03fc4cfa3bcb7ed167e5f1ac79d4f2b6c9f10a414e6946497d8 1000000000 0xae8da2da7d08b7591709a6e6ff95e1a64067526087749aa4ecf8bb5cb4b8e273
-
-
-sui client call --package 0x2 \
-  --module coin \
-  --function mint_and_transfer \
-  --type-args 0xa86b066a51f60c6c86b20f61d3c17b58be469ded3c8068837c4a6d7106f66761::yizuo_rmb::YIZUO_RMB \
-   --args   0xd057f05afd299294f21e9764a97e5b69d866a15c25cb753cc49d7a1969194072 1000000000 0xae8da2da7d08b7591709a6e6ff95e1a64067526087749aa4ecf8bb5cb4b8e273
